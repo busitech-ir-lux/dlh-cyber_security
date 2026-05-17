1203b40d35b7612c352c29ae37be9410b0114cc1
Mahdi <info.busitech@proton.me>
2026-05-17 18:34:54 -0500

    Crack hashed passwords

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt --type=raw-md5 $1 && john --show $1 > 4-password.txt   
+john --format=Raw-MD5 --wordlist=/usr/share/wordlists/rockyou.txt $1 && john --show hash.txt > 4-password.txt
