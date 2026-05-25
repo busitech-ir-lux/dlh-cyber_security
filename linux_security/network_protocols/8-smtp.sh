bd9667d950331abb46ae4a1abe971b94c9ca3e00
Mahdi <info.busitech@proton.me>
2026-05-25 10:19:09 -0500

    Check SMTP configuration for TLS

--- linux_security/network_protocols/8-smtp.sh
+++ linux_security/network_protocols/8-smtp.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-CONFIG="/etc/postfix/main.cf"
-if grep -q "^smtpd_tls_security_level" "$CONFIG"; then
-    grep "^smtpd_tls_security_level" "$CONFIG"
-else
-    echo "STARTTLS not configured"
-fi
+if grep -q "^smtpd_tls_security_level"; echo "CONFIG"
