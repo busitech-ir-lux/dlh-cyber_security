c3b7ae9cf1b0273df77b021eaf534d6defea10b5
Mahdi <info.busitech@proton.me>
2026-06-01 17:03:31 -0500

    Return status code and headers of a given url

--- scripting_cyber/0x01-python_scripting/3-http_headers.py
+++ scripting_cyber/0x01-python_scripting/3-http_headers.py
@@ -6,7 +6,7 @@ def get_http_headers(url):
     result = {}
     try:
         response = requests.get(url)
-        result = {response.status_code, response.headers}
+        result = {['Status Code: ': response.status_code],['Headers: ', response.headers]}
         return result
     except requests.exceptions.RequestException as e:
         return f"Error: {e}"
