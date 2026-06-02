33ec3ccabb6e4638d25a9ada5fe64f55509523d8
Mahdi <info.busitech@proton.me>
2026-06-02 08:27:44 -0500

    Web recon advanced

--- scripting_cyber/0x01-python_scripting/6-netrecon.py
+++ scripting_cyber/0x01-python_scripting/6-netrecon.py
@@ -13,38 +13,25 @@ def dns_recon(domain):
 
     try:
         ip = socket.gethostbyname(domain)
-        mx_records = dns.resolver.resolve(domain, 'MX')
-        mx_records = [str(record.exchange) for record in mx_records]
+        print(f"IP Address: {ip}")
+        
     except socket.gaierror:
         return f"Error resolving {domain}: Host not found"
-    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.NoNameservers):
-        mx_records = ['No MX records found']
-    mx_list = '\n    '.join(mx_records)
-    return f"""
-IP Address: {ip}
-
-
-MX Records: 
-    {mx_list}"""
 
 def web_recon(domain):
     
     try:
-        response = requests.get(f"https://{domain}", timeout=5)
-    except requests.RequestException as e:
-        return f"Error fetching {domain}: {e}"
-    except:
         response = requests.get(f"http://{domain}", timeout=5)
+        print(f"    Status Code: {response.status_code}")
+        
+        for key in ['Content-Type', 'Server', 'X-Frame--Options']:
+            if key in response.headers:
+                print(f"    {key}: {response.headers[key]}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        print(f"    Total Links Found: {len(soup.find_all('a', href=True))}")
+    except requests.exceptions.RequestException as e:
+        return f"Error fetching {domain}: {e}"
 
-    status_code = response.status_code
-    total_links = len(BeautifulSoup(response.text, "html.parser").find_all('a'))
-    return f"""Status Code: {status_code}
-Important Headers:
-    Content-Type: {response.headers.get('Content-Type', 'N/A')}
-    Server: {response.headers.get('Server', 'N/A')}
-
-    
-Total Links Found: {total_links}"""
 
 
 def port_scan(domain):
