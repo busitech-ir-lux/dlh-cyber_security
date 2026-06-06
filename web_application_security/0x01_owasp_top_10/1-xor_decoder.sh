131bf4ce48fc0c8ed46ed4213d74d420f917ff50
Mahdi <info.busitech@proton.me>
2026-06-06 08:28:44 -0500

    XOR WebSphere decoder

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo $1 | sed 's/{xor}//' | base64 -d
+echo "KzosKw==" | base64 -d | python3 -c '
+import sys
+data = sys.stdin.buffer.read()
+
+key = 0x5F
+
+print(bytes([b ^ key for b in data]).decode())
+'
