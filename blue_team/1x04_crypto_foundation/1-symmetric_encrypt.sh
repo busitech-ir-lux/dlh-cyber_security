e574997c38017c400b77eb8ca3c083c518915dd1
busitech <info.busitech@proton.me>
2026-07-27 13:25:39 -0400

    The Cryptographic Foundation - The Symmetric Engine

--- blue_team/1x04_crypto_foundation/1-symmetric_encrypt.sh
+++ blue_team/1x04_crypto_foundation/1-symmetric_encrypt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+## If the input includes CBC, this script encrypts in cbc mode, and if it
+## includes gcm, it encrypts in gcm mode
 openssl enc -aes-256-$3 -salt -pbkdf2 -iter 200000 -in $1 -out $2
 
