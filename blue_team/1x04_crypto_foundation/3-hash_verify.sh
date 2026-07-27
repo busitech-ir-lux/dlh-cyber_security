6580f3ecf0983ea6c0b1750added5ca59d8ac375
busitech <info.busitech@proton.me>
2026-07-27 15:36:06 -0400

    The Cryptographic Foundation - The Hash Laboratory

--- blue_team/1x04_crypto_foundation/3-hash_verify.sh
+++ blue_team/1x04_crypto_foundation/3-hash_verify.sh
@@ -3,6 +3,6 @@ inHash=$2
 calHash=$(sha256sum $1 | cut -d ' ' -f1)
 if [ $inHash == $calHash ]; then
 	echo "INTEGRITY OK"; exit 0
-else echo "INTEGRITY FAILED - EXPECTED {$calHash} got {$inHash}"; exit 1
+else echo "INTEGRITY FAILED - expected {$calHash} got {$inHash}"; exit 1
 fi
 
