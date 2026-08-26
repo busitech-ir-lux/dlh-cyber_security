de359193233bede754def97ab7a087cdc3c84cba
Mahdi <info.busitech@proton.me>
2026-08-26 12:51:55 +0200

    Revert "The Defensible Endpoint Package - The Telemetry Instrumentation"
    This reverts commit d6cdabd6f4bb70ead6c7e68680bc9f10ac6d8e61.

--- blue_team/2x05_defensible_endpoint/5-telemetry_deploy.ps1
+++ blue_team/2x05_defensible_endpoint/5-telemetry_deploy.ps1
@@ -84,7 +84,7 @@ function Verify-Event {
             }
         }
         catch {
-            # Ignore malformed/unparseable event records and continue. (expected event)
+            # Ignore malformed/unparseable event records and continue. (expected record)
         }
     }
 
